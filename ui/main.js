b10bad44ecf93ba38794e397e27740122c76bf0c
hmkarthik <30756767+hmkarthik@users.noreply.github.com>
2017-08-16 08:44:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the text of div element main-text
 var element = document.getElementById('main-text');
 element.innerHTML = "I'm learning HTML,CSS and Javascript";
+
+//move the image
+var img = document.getElementById('main-img');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
