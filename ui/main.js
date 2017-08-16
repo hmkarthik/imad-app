d0bd934d1eb63b230fcd8579e467377414e3611a
hmkarthik <30756767+hmkarthik@users.noreply.github.com>
2017-08-16 08:50:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML = "I'm learning HTML,CSS and Javascript";
 
 //move the image
 var img = document.getElementById('main-img');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,50);
 };
