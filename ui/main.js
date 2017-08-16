0b0e7a6854eb8dc8fabc6d5024a9aa185c768941
hmkarthik <30756767+hmkarthik@users.noreply.github.com>
2017-08-16 08:35:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,5 @@
 console.log('Loaded!');
+
+//change the text of div element main-text
+var element = document.getElementById('main-text');
+element.innerHtml = "I'm learning HTML,CSS and Javascript";
