e09111ca571ef710956325b6e10e673cbbb2159d
hmkarthik <30756767+hmkarthik@users.noreply.github.com>
2017-08-20 18:03:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,9 +131,9 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
     //get the name from the request
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     //JSON : Javascript Object Notation
     res.send(JSON.stringify(names));
