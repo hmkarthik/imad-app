75ecff6d4fbd3c65d65ca3a6562390707f07f213
hmkarthik <30756767+hmkarthik@users.noreply.github.com>
2017-08-22 00:41:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,6 +136,21 @@ app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
 });
 
 app.get('/articles/:articleName', function (req, res) {
+   pool.query("SELECT * FROM article1 WHERE title = '" +res.params.articleName + "'", function(err,result) {
+      if(err) {
+          res.status(500).send(err.toString());
+      } else {
+          if(result.rows.length === 0) {
+              res.status(404).send('Article not found');
+          } else {
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+   });
+});
+
+/*app.get('/articles/:articleName', function (req, res) {
     pool.query("SELECT * FROM article1 WHERE title = '" + res.params.articleName + "'", function(err, result) {
       if(err) {
           res.status(500).send(err.toString());
@@ -148,7 +163,7 @@ app.get('/articles/:articleName', function (req, res) {
           }
       }        
     });
-});
+});*/
 
 app.get('/article-two', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
